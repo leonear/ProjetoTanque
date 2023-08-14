4c5f09208af6db6269ceab708731a99a0d52ecd7
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-08-14 19:55:11 -0300

    criação de código para teste do pygame

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame;
+
+pygame.init() #inicia diversas funcionalidades do pygame
+
+#retorna uma tela pronta com a dimensão passada por parametro
+screen = pygame.display.set_mode((600, 400))
+
+
+loop = True
+while loop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT
+
+
+    screen.fill((255,255,255)) #preenche a tela com uma determinada área
+    pygame.display.flip() #atualiza o display
