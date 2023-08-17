0a6161bd6a8f362455cc038633ff011968d7e9eb
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-08-17 17:53:57 -0300

    tentativa de girar o tanque

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ pygame.init() #inicia diversas funcionalidades do pygame
 SCREEN_SIZE = (800,600)
 screen = pygame.display.set_mode(SCREEN_SIZE, 0 ,32) #retorna uma tela pronta com a dimensão passada por parametro
 
-tank = pygame.image.load('tanque.png').convert()
+tank = pygame.image.load('tanque.png').convert_alpha()
+tank = pygame.transform.flip(tank, False, True)
 DEFAULT_IMAGE_SIZE = (200, 100)
 tank = pygame.transform.scale(tank, DEFAULT_IMAGE_SIZE)
 
 x,y = 0,0
 move_x, move_y = 0,0
+rotation = 0  # Ângulo de rotação do tanque
+
 
 
 while True:
@@ -19,50 +22,42 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+        #if event.type == pygame.MOUSEMOTION:
+        #    (mouseX, mouseY) = pygame.mouse.get_pos()
+        #    move_x = mouseX
+        #    move_y = mouseY
 
-        if event.type == pygame.MOUSEMOTION:
-           (mouseX, mouseY) = pygame.mouse.get_pos()
-           move_x = mouseX
-           move_y = mouseY
+        keys = pygame.key.get_pressed()
+        if event.type == KEYDOWN:
+            if event.key==K_LEFT:
+                move_x=-10
+                rotation = 180
 
+            if event.key==K_RIGHT:
+                move_x=+10
+                rotation = 0
+            if event.key == K_UP:
+                move_y=-10
+            if event.key == K_DOWN:
+               move_y=+10
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                move_x=0
+            if event.key == K_RIGHT:
+               move_x=0
+            if event.key == K_UP:
+                move_y=0
+            if event.key == K_DOWN:
+                move_y=0
 
+        x += move_x
+        y += move_y
 
-        #if event.type == KEYDOWN:
-        #    if event.key==K_LEFT:
-        #        move_x=-10
-        #if event.type == KEYUP:
-        #    if event.key== K_LEFT:
-        #        move_x = 0
+#        x = move_x
+#        y = move_y
 
-        #if event.type == KEYDOWN:
-        #    if event.key==K_RIGHT:
-        #        move_x= +10
-        #if event.type == KEYUP:
-        #    if event.key== K_RIGHT:
-        #        move_x = 0
-
-        #if event.type == KEYDOWN:
-        #    if event.key == K_UP:
-        #        move_y = -10
-        #if event.type == KEYUP:
-        #    if event.key == K_UP:
-        #        move_y = 0
-
-        #if event.type == KEYDOWN:
-        #    if event.key == K_DOWN:
-        #        move_y = +10
-        #if event.type == KEYUP:
-        #    if event.key == K_DOWN:
-        #        move_y = 0
-
-
-
-
-        x = move_x
-        y = move_y
-
-        screen.fill((255, 255, 255)) #preenche a tela com uma determinada área
-        screen.blit(tank,(x,y))
-
-
-        pygame.display.update()#atualiza o display
+        screen.fill((255,255,255))
+        rotated_tank = pygame.transform.rotate(tank, rotation)
+        tank_rect = rotated_tank.get_rect(center=(x, y))
+        screen.blit(rotated_tank, tank_rect.topleft)
+        pygame.display.update()
