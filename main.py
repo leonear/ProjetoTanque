7d239549a618bd1298c7f1505d09ca0084530783
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-08-17 20:22:52 -0300

    Entrega do lab 02

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Leonardo Medeiros Silva Aparicio
 import pygame
 from pygame.locals import *
 from sys import exit
@@ -9,12 +10,15 @@ screen = pygame.display.set_mode(SCREEN_SIZE, 0 ,32) #retorna uma tela pronta co
 tank = pygame.image.load('tanque2.png').convert_alpha()
 DEFAULT_IMAGE_SIZE = (240, 240)
 tank = pygame.transform.scale(tank, DEFAULT_IMAGE_SIZE)
+
+#----- Tentativa com a primeira imagem -----
 #tank_flip = pygame.transform.flip(tank, True, False)
+#tank_image = tank
 
 x,y = 0,0
 move_x, move_y = 0,0
 rotation = 0  # Ângulo de rotação do tanque
-#tank_image = tank
+
 
 
 
@@ -24,23 +28,24 @@ while True:
             pygame.quit()
             exit()
 
-        # if event.type == pygame.MOUSEMOTION:
-        #    (mouseX, mouseY) = pygame.mouse.get_pos()
-        #    move_x = mouseX
-        #    move_y = mouseY
+#----------Movendo o tanque com o mouse!-----------
+#        if event.type == pygame.MOUSEMOTION:
+#           (mouseX, mouseY) = pygame.mouse.get_pos()
+#           move_x = mouseX
+#           move_y = mouseY
 
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
-                move_x = -2
+                move_x = -1
                 rotation = 90
             if event.key == K_RIGHT:
-                move_x = 2
+                move_x = 1
                 rotation = -90
             if event.key == K_UP:
-                move_y = -2
+                move_y = -1
                 rotation = 0
             if event.key == K_DOWN:
-                move_y = 2
+                move_y = 1
                 rotation = 180
         if event.type == KEYUP:
             if event.key == K_LEFT or event.key == K_RIGHT:
@@ -50,8 +55,9 @@ while True:
 
     x += move_x
     y += move_y
-    #x = move_x código para o movimento com o mouse
-    #y = move_y código para o movimento com o mouse
+
+#   x = move_x #código para o movimento com o mouse
+#   y = move_y #código para o movimento com o mouse
 
     screen.fill((255, 255, 255))
     rotated_tank = pygame.transform.rotate(tank, rotation)
